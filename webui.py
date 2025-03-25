8050383a4244432fb66d52ac90c261f8f1cf35ea
iDhanush <idhanushm@gmail.com>
2025-03-26 00:29:50 +0530

    udpate

--- webui.py
+++ webui.py
@@ -170,7 +170,7 @@ def get_models():
 
 # Add more endpoints for style handling, configuration, etc.
 
-if __name__ == "__main__":
-    import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+import uvicorn
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
